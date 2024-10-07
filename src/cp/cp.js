faf014530156fa56838ee84af31bc20bae4b29a2
Aminov <alami@tshtt.uz>
2024-10-07 20:53:12 +0500

    add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from 'node:child_process';
+import { stdin, exit } from 'node:process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const ls = spawn('node', ['files//script.js', ...args]);
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+    ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+    ls.on('close', (code) => {
+        console.log(`Cahild process finished with exit code ${code}`);
+        exit()
+    });
+    stdin.on('data', (ch)=>{
+        ls.stdin.write(ch);
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(  ['someArgument'] );
